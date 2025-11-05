18b30efbe37c345a1054d2fd401d4023b89dea7c
ponnow <ponnow.nlr@gmail.com>
2025-11-06 00:43:46 +0600

    Add project files.

---
+++ StudentManagement/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagement.Models;
+
+namespace StudentManagement
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ StudentManagement/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagement.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required,MaxLength(100)]
+        public string Name { get; set; }
+        [Required,EmailAddress]
+        public string Email { get; set; }
+        [Required,MaxLength(100)]
+        public string Department { get; set; }
+    }
+}
